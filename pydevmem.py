9136cfbbf50222e8b209c400327daeca441d8a74
Kyle Manna <kyle@kylemanna.com>
2012-06-24 21:55:56 -0500

    pydevmem: Add getitem and setitem
    This allows easy access in to the buffer using [] operator.  Future
    enhancements will throw IndexError and TypeError.

--- pydevmem.py
+++ pydevmem.py
@@ -32,6 +32,12 @@ class DevMemBuffer:
     def __len__(self):
         return len(self.data)
 
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
+
     def hexdump(self, word_size = 4, words_per_row = 4):
         # Build a list of strings and then join them in the last step.
         # This is more efficient then concat'ing immutable strings.
