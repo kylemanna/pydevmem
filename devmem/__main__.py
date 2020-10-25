6da2ad3d106f4a2ddda5bb2b2d17ff89e8864312
Kyle Manna <kyle@kylemanna.com>
2020-10-25 14:17:48 -0700

    devmem: main: Convert to argparse and main()
    * Move to a function and switch to argparse.
    * Fix setup.py

--- devmem/__main__.py
+++ devmem/__main__.py
@@ -1,115 +1,132 @@
 #!/usr/bin/env python3
 
-import optparse
+import argparse
+import os
 import sys
+
 from . import DevMem
 
 """If this is run as a script (rather then imported as a module) it provides some basic functionality out of the box"""
 
-parser = optparse.OptionParser()
+# Need to fix up argparse's prog value as it's broken and returns `__main__.py`
+# Upstream bug:
+# * https://bugs.python.org/issue22240
+# pip workaround:
+# * https://github.com/pypa/pip/blob/08c99b6e00135ca8df2e98db58aa0b701b971c64/src/pip/_internal/utils/misc.py#L124-L134
+def get_prog() -> str:
+    """Determine the program name if invoked directly or as a module"""
 
-parser.add_option(
-    "-r", "--read", dest="read", metavar="ADDR", type=int, help="read a value"
-)
+    name = (
+        sys.argv[0]
+        if globals().get("__spec__") is None
+        else __spec__.name.partition(".")[0]
+    )
+    try:
+        prog = os.path.basename(sys.argv[0])
+        if prog in ("__main__.py", "-c"):
+            return "{} -m {}".format(sys.executable, name)
+        else:
+            return prog
+    except (AttributeError, TypeError, IndexError):
+        pass
 
-parser.add_option(
-    "-w",
-    "--write",
-    dest="write",
-    help="write a value",
-    nargs=2,
-    type=int,
-    metavar="ADDR VALUE",
-)
+    return name
 
-parser.add_option(
-    "-n", "--num", dest="num", help="number of words to read", type=int, default=1
-)
 
-parser.add_option(
-    "-s",
-    "--word-size",
-    dest="word_size",
-    help="size of word when displayed",
-    type=int,
-    default=4,
-)
+def main() -> int:
+    """Main function with useful demo application"""
 
-parser.add_option(
-    "-m",
-    "--mmap",
-    dest="mmap",
-    metavar="FILE",
-    help="file to open with mmap()",
-    type=str,
-    default="/dev/mem",
-)
+    parser = argparse.ArgumentParser(prog=get_prog())
 
-parser.add_option(
-    "-v",
-    action="store_true",
-    dest="verbose",
-    help="provide more information regarding operation",
-)
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        "-r", "--read", metavar="ADDR", type=lambda x: int(x, 0), help="read a value"
+    )
+    group.add_argument(
+        "-w",
+        "--write",
+        help="write a value",
+        nargs=2,
+        type=lambda x: int(x, 0),
+        metavar=("ADDR", "VALUE"),
+    )
 
-parser.add_option(
-    "-d", action="store_true", dest="debug", help="provide debugging information"
-)
+    parser.add_argument(
+        "-n",
+        "--num",
+        help="number of words to read",
+        type=lambda x: int(x, 0),
+        default=1,
+    )
 
-(options, args) = parser.parse_args()
+    parser.add_argument(
+        "-s",
+        "--word-size",
+        help="size of word when displayed",
+        type=int,
+        default=4,
+        choices=[1, 2, 4],
+    )
 
-# Check for sane arguments
-if options.write is not None and options.read is not None:
-    parser.print_help()
-    print("\nError: Both read and write are specified")
-    sys.exit(1)
-elif options.write is None and options.read is None:
-    parser.print_help()
-    print("\nError: Neither read or write are specified")
-    sys.exit(1)
+    parser.add_argument(
+        "-m",
+        "--mmap",
+        metavar="FILE",
+        help="file to open with mmap()",
+        type=str,
+        default="/dev/mem",
+    )
 
-if options.num < 0:
-    parser.print_help()
-    print("\nError: Invalid num of words specified")
-    sys.exit(1)
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="provide more information regarding operation",
+    )
 
-if options.word_size != 1 and options.word_size != 2 and options.word_size != 4:
-    parser.print_help()
-    print("\nError: Invalid word size specified")
-    sys.exit(1)
+    parser.add_argument(
+        "-d", "--debug", action="store_true", help="provide debugging information"
+    )
 
-# Only support writing one word at a time, force this
-if options.write is not None and options.num != 1:
-    print("Warning: Forcing number of words to 1 for set operation\n")
-    options.num = 1
+    args = parser.parse_args()
 
-# Determine base address to operate on
-addr = options.read
-if options.write is not None:
-    addr = options.write[0]
+    # Check for sane arguments
+    if args.num < 0:
+        parser.print_help()
+        print("\nError: Invalid num of words specified")
+        return 1
 
-# Create the Dev Mem object that does the magic
-mem = DevMem(addr, length=options.num, filename=options.mmap, debug=options.debug)
+    # Only support writing one word at a time, force this
+    if args.write is not None and args.num != 1:
+        print("Warning: Forcing number of words to 1 for set operation\n")
+        args.num = 1
 
-if options.debug:
-    mem.debug_set(1)
+    # Determine base address to operate on
+    addr = args.write[0] if args.write else args.read
 
-# Perform the actual read or write
-if options.write is not None:
-    if options.verbose:
-        print(
-            "Value before write:\t{0}".format(
-                mem.read(0x0, options.num).hexdump(options.word_size)
+    # Create the Dev Mem object that does the magic
+    mem = DevMem(addr, length=args.num, filename=args.mmap, debug=args.debug)
+
+    # Perform the actual read or write
+    if args.write is not None:
+        if args.verbose:
+            print(
+                "Value before write:\t{0}".format(
+                    mem.read(0x0, args.num).hexdump(args.word_size)
+                )
             )
-        )
 
-    mem.write(0x0, [options.write[1]])
+        mem.write(0x0, [args.write[1]])
 
-    if options.verbose:
-        print(
-            "Value after write:\t{0}".format(
-                mem.read(0x0, options.num).hexdump(options.word_size)
+        if args.verbose:
+            print(
+                "Value after write:\t{0}".format(
+                    mem.read(0x0, args.num).hexdump(args.word_size)
+                )
             )
-        )
-else:
-    print(mem.read(0x0, options.num).hexdump(options.word_size))
+    else:
+        print(mem.read(0x0, args.num).hexdump(args.word_size))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
